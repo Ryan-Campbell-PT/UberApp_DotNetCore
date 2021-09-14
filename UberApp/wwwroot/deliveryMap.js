c58f4eee47ecd7f195354e01812026c77298a750
Ryan Campbell <ryacampbell15@gmail.com>
2021-09-14 09:12:39 -0500

    Initial commit

---
+++ UberApp/wwwroot/deliveryMap.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    var tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var tileAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
+
+    // Global export
+    window.deliveryMap = {
+        showOrUpdate: function (elementId, markers) {
+            var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'pk.eyJ1IjoicmNhbXBiMjciLCJhIjoiY2t0ajJvbnc2MTdydjJvcW5hYzhhbG9qaiJ9.IElatfLFvnK-MnrJyp67sQ'
+            }).addTo(mymap);
+
+        }
+    };
+
+    function animateMarkerMove(marker, coords, durationMs) {
+        if (marker.existingAnimation) {
+            cancelAnimationFrame(marker.existingAnimation.callbackHandle);
+        }
+
+        marker.existingAnimation = {
+            startTime: new Date(),
+            durationMs: durationMs,
+            startCoords: { x: marker.getLatLng().lng, y: marker.getLatLng().lat },
+            endCoords: coords,
+            callbackHandle: window.requestAnimationFrame(() => animateMarkerMoveFrame(marker))
+        };
+    }
+
+    function animateMarkerMoveFrame(marker) {
+        var anim = marker.existingAnimation;
+        var proportionCompleted = (new Date().valueOf() - anim.startTime.valueOf()) / anim.durationMs;
+        var coordsNow = {
+            x: anim.startCoords.x + (anim.endCoords.x - anim.startCoords.x) * proportionCompleted,
+            y: anim.startCoords.y + (anim.endCoords.y - anim.startCoords.y) * proportionCompleted
+        };
+
+        marker.setLatLng([coordsNow.y, coordsNow.x]);
+
+        if (proportionCompleted < 1) {
+            marker.existingAnimation.callbackHandle = window.requestAnimationFrame(
+                () => animateMarkerMoveFrame(marker));
+        }
+    }
+})();
